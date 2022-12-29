0ffa78d4a11469af46ef4b8ae52a6f5aa8b613c5
Fifus17 <filipdziurdzia2002@gmail.com>
2022-12-29 10:50:38 +0100

    working on the database connection service

--- src/app/Services/database-connection.service.ts
+++ src/app/Services/database-connection.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
-import { Observable } from 'rxjs';
+import { first, Observable } from 'rxjs';
 import { Trip } from '../Interfaces/ITrip';
+import { User } from '../Interfaces/IUser';
 
 @Injectable({
   providedIn: 'root'
@@ -27,5 +28,47 @@ export class DatabaseConnectionService {
 
   addTrip(trip: Trip): void {
     this.db.list('trips').push(trip);
+  }
+
+  removeTrip(idx: number): void {
+    this.db.list('trips').snapshotChanges().pipe(first()).subscribe((trips: any) => {
+      for (let trip of trips) {
+        if (trip.payload.val().id === idx) {
+          this.db.list('trips').remove(trip.payload.key);
+          break;
+        }
+      }
+    });
+  }
+
+  modifyTrip(trip: Trip): void {
+    this.db.list('trips').snapshotChanges().pipe(first()).subscribe((trips: any) => {
+      for (let t of trips) {
+        if (t.payload.val().id === trip.id) {
+          this.db.list('trips').update(t.payload.key, trip);
+          break;
+        }
+      }
+    });
+  }
+
+  getNextId(): number | undefined {
+    return this.nextId;
+  }
+
+  getUsers(): Observable<any[]> {
+    return this.db.list<User>('users').valueChanges();
+  }
+
+  addUser(user: User): void {
+    this.db.list('users/' + user.uid).push(user);
+  }
+
+  changeUserRoles(uid: string, roles: string[]): void {
+    this.db.list('users/' + uid).update('roles', roles);
+  }
+
+  getOrderHistory(uid: string): Observable<any[]> {
+    return this.db.list('users/' + uid + '/orders').valueChanges();
   }
 }
